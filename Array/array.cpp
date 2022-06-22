b2eef7b63f62f2e48e985db1764477ba9c491197
Dhaval Kolhe <72658475+dhavalkolhe@users.noreply.github.com>
2022-06-22 18:56:56 +0530

    Array - 1480

--- Array/array.cpp
+++ Array/array.cpp
@@ -14,44 +14,89 @@
 // }
 
 // !1929 - Array - Concatenation of Array
+// ? Learned How to dynamically assign values in Vector, 
+// ? and how to concatenate arrays.
 
-#include <iostream>
-#include <vector>
+// #include <iostream>
+// #include <vector>
 
-using namespace std;
+// using namespace std;
 
-int main ()
-{
-  std::vector<int> nums;
-  std::vector<int> ans;
-  int a, n=2;
+// int main ()
+// {
+//   std::vector<int> nums;
+//   std::vector<int> ans;
+//   int a, n=2;
 
-  std::cout << "Please enter some integers:\n";
+//   std::cout << "Please enter some integers:\n";
 
-    for (int i=0; i<n; i++)
-    {
-        std::cin >> a;
-        nums.push_back(a);
-    }
+//     for (int i=0; i<n; i++)
+//     {
+//         std::cin >> a;
+//         nums.push_back(a);
+//     }
 
-    cout << "The nums array is: ";
-    for (int i = 0; i < n; i++)
-    {
-        std::cout << nums[i] << " ";
-    }
+//     cout << "The nums array is: ";
+//     for (int i = 0; i < n; i++)
+//     {
+//         std::cout << nums[i] << " ";
+//     }
 
-    ans.assign(nums.begin(), nums.end()); 
+//     ans.assign(nums.begin(), nums.end()); 
 
-    for(int j=0; j< n; j++)
-    {
-        ans.push_back(nums[j]);
-    }
+//     for(int j=0; j< n; j++)
+//     {
+//         ans.push_back(nums[j]);
+//     }
 
-    cout << "\nThe ans array is: ";
-    for (int j = 0; j < 2*n; j++)
-    {
-        std::cout << ans[j] << " ";
-    }
+//     cout << "\nThe ans array is: ";
+//     for (int j = 0; j < 2*n; j++)
+//     {
+//         std::cout << ans[j] << " ";
+//     }
     
-  return 0;
-}
+//   return 0;
+// }
+
+// !1480 - Array -  Running Sum of 1d Array
+
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// int main(){
+//     std::vector<int> nums;
+//     std::vector<int> runningsum;
+    
+//     int a, n=3, sum = 0;
+
+//       std::cout << "Please enter some integers:\n";
+
+//     for (int i=0; i<n; i++)
+//     {
+//         std::cin >> a;
+//         nums.push_back(a);
+//     }
+
+//     cout << "The nums array is: ";
+//     for (int i = 0; i < n; i++)
+//     {
+//         std::cout << nums[i] << " ";
+//     }
+
+//     // Logic to calculate running sum
+//     for(int i=0; i<n; i++)
+//     {
+//         sum += nums[i];
+//         runningsum.push_back(sum);
+//     }
+
+//     cout << "The running sum array is: ";
+//     for (int i = 0; i < n; i++)
+//     {
+//         std::cout << runningsum[i] << " ";
+//     }
+
+// }
+
